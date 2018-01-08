80687188f9da84d9a355d8e5c6be8c9fcacc23b8
mithsin <hsinbluemoon@gmail.com>
2018-01-08 18:32:54 -0500

    stuff

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -108,8 +108,8 @@ class Main extends Component {
         <div className="container">
           {/* Jumbotron */}
           <div className="jumbotron">
-            <h1 className="text-center"><strong>New York Times Article Search</strong></h1>
-            <h2 className="text-center">Search for and save articles of interest.</h2>
+            <h1 className="text-center"><strong>New York Times Search</strong></h1>
+            <h2 className="text-center">search articles</h2>
           </div>
           {/* Search Form and Results Section */}
           <Search
--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 const Results = props =>
-  // <div>
-  //   <h4>{props.title}</h4>
-  //   <p>Publish Date & Time: {props.date}</p>
-  //   <a href={props.url} target="_blank">Link To Article</a>
-  //   <button onClick={() => props.handleSaveButton(props._id)}>Save</button>
-  // </div>
+
   <div className="container">
     <li className="list-group-item">
       <h4>
